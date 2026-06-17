8988d7621f5c6e815e14563b292bc9ea03990c28
harith <147239398+SaiHarithK@users.noreply.github.com>
2026-06-17 08:53:48 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,30 +1,33 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
- import org.openqa.selenium.By;
-  import org.openqa.selenium.WebDriver;
-  import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException
+    public static void main(String[] args)
     {
-        WebDriver driver = new ChromeDriver();
-        
-        driver.manage().window().maximize();
+        //  Add this block
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");   // required for Jenkins
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        //  Replace driver creation
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
-        Thread.sleep(2000);
-        //https://support.saucelabs.com/s/login/?language=en_US
+        
+        //  maximize may fail in headless, so optional
+        // driver.manage().window().maximize();
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        
         driver.findElement(By.id("login-button")).click();
-        
-        Thread.sleep(3000);
-        
+
+        // good practice
         driver.quit();
     }
 }
